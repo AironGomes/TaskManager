0fb141c9d6cb94306647444b89269b609ac477a5
Airon Gomes <airongomes@byui.edu>
2021-02-08 21:08:50 -0300

    Changed position of the menu items.

--- app/src/main/java/com/airongomes/taskmanager/MainActivity.kt
+++ app/src/main/java/com/airongomes/taskmanager/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.drawerlayout.widget.DrawerLayout.*
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.airongomes.taskmanager.databinding.ActivityMainBinding
-import com.airongomes.taskmanager.util.hideKeyboard
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DrawerLockMode {
 
-    private lateinit var drawerLayout: DrawerLayout
+    lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +24,27 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         NavigationUI.setupWithNavController(binding.navView, navController)
+
     }
 
+    /**
+     * Support Navigation Up
+     */
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.nav_host_fragment)
         return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
+    /**
+     * Define if drawer swiping gesture is locked or unlocked
+     */
+    override fun setDrawerLocked(locked: Boolean) {
+        drawerLayout.setDrawerLockMode(
+                if (locked) {
+                    LOCK_MODE_LOCKED_CLOSED
+                }
+                else{
+                    LOCK_MODE_UNLOCKED
+                })
     }
 }
